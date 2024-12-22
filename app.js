128f6043d6e0fdb5aac6f845ed96d20af0f1193e
aaadhilll <aratthennala@gmail.com>
2024-12-22 15:14:22 +0530

    project table create and insert api done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: `${process.cwd()}/.env` });
 const express = require('express');
 const catchAsync = require('./utils/catchAsync');
 const authRouter = require('./router/authRoute');
+const prodjectRouter = require('./router/projectRoute');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 const { stack } = require('sequelize/lib/utils');
@@ -18,6 +19,7 @@ app.use(express.json());
 // all routes will be here //
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/projects', prodjectRouter);
 
 const PORT = process.env.APP_PORT || 4000;
 
---
+++ router/projectRoute.js
@@ -0,0 +1,8 @@
+const { createProject } = require('../controller/projectController');
+
+
+const router = require('express').Router();
+
+router.route('/').post(createProject)
+
+module.exports = router;
