dad713c3558ddde02778453dc287060991cb5468
aaadhilll <aratthennala@gmail.com>
2024-12-22 23:36:04 +0530

    update, delete(current user)

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,5 +1,6 @@
 const project = require("../db/models/project");
 const user = require("../db/models/user");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 const createProject = catchAsync(async (req, res, next) => {
@@ -40,10 +41,65 @@ const getProjectById = catchAsync(async (req, res, next) => {
     const projectId = req.params.id; 
     const result = await project.findByPk(projectId, { include: user });
 
+    if (!result) {
+        return next(new AppError('invalid project id', 400));
+    }
+
     return res.json({
         status: 'success',
         data: result,
     });
 });
 
-module.exports = { createProject, getAllProject, getProjectById };
+const updateProject = catchAsync(async (req, res, next) => {
+    const userId = req.user.id;
+    const projectId = req.params.id;
+    const body = req.body;
+
+    const result = await project.findOne({
+        where: { id: projectId, createdBy: userId },
+    });
+
+    if (!result) {
+        return next(new AppError('Invalid project id', 400));
+    }
+
+    result.title = body.title;
+    result.productImage = body.productImage;
+    result.price = body.price;
+    result.shortDescription = body.shortDescription;
+    result.description = body.description;
+    result.productUrl = body.productUrl;
+    result.category = body.category;
+    result.tags = body.tags;
+
+    const updatedResult = await result.save();
+
+    return res.json({
+        status: 'success',
+        data: updatedResult,
+    });
+});
+
+const deleteProject = catchAsync(async (req, res, next) => {
+    const userId = req.user.id;
+    const projectId = req.params.id;
+    const body = req.body;
+
+    const result = await project.findOne({
+        where: { id: projectId, createdBy: userId },
+    });
+
+    if (!result) {
+        return next(new AppError('Invalid project id', 400));
+    }
+
+    await result.destroy();
+
+    return res.json({
+        status: 'success',
+        message: 'Record deleted successfully',
+    });
+});
+
+module.exports = { createProject, getAllProject, getProjectById, updateProject, deleteProject };
--- router/projectRoute.js
+++ router/projectRoute.js
@@ -1,11 +1,13 @@
 const { authentication, restrictTo } = require('../controller/authCotroller');
-const { createProject, getAllProject, getProjectById } = require('../controller/projectController');
+const { createProject, getAllProject, getProjectById, updateProject, deleteProject } = require('../controller/projectController');
 
 
 const router = require('express').Router();
 
 router.route('/').post(authentication, restrictTo('1'), createProject).get(authentication, getAllProject);
 
-router.route('/:id').get(authentication, getProjectById);
+router.route('/:id').get(authentication, getProjectById).patch(authentication, updateProject).delete(authentication, deleteProject);
+
+
 
 module.exports = router;
