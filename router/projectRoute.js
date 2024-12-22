0fe9f27fcca2e484496c22ba87f59ef84f3068a0
aaadhilll <aratthennala@gmail.com>
2024-12-22 20:08:51 +0530

    autherization done

--- router/projectRoute.js
+++ router/projectRoute.js
@@ -1,9 +1,9 @@
-const { authentication } = require('../controller/authCotroller');
+const { authentication, restrictTo } = require('../controller/authCotroller');
 const { createProject } = require('../controller/projectController');
 
 
 const router = require('express').Router();
 
-router.route('/').post(authentication,createProject)
+router.route('/').post(authentication, restrictTo('2'), createProject)
 
 module.exports = router;
