d1a7b4dc947fa8115e042c027e66d14564872f25
aaadhilll <aratthennala@gmail.com>
2024-12-22 19:57:31 +0530

    authentication done

--- router/projectRoute.js
+++ router/projectRoute.js
@@ -1,8 +1,9 @@
+const { authentication } = require('../controller/authCotroller');
 const { createProject } = require('../controller/projectController');
 
 
 const router = require('express').Router();
 
-router.route('/').post(createProject)
+router.route('/').post(authentication,createProject)
 
 module.exports = router;
